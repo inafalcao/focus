8d6635f89fbbd8f2dcede3bd6ab03600ef1819fb
inafalcao <inafalcao@gmail.com>
2023-02-17 19:23:10 -0300

    Creating a separate component just for the count down visualization.

--- focus-react-client/src/pages/Home/HomeStyles.ts
+++ focus-react-client/src/pages/Home/HomeStyles.ts
@@ -57,36 +57,15 @@ export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
 `
 
-export const CountDownContainer = styled.div`
-  font-family: 'Roboto Mono', monospace;
-  font-size: 10rem;
-  line-height: 8rem;
-  color: ${(props) => props.theme['gray-100']};
-
+export const ButtonsContainer = styled.div`
   display: flex;
-  gap: 1rem;
-
-  span {
-    background-color: ${(props) => props.theme['gray-700']};
-    padding: 2rem 1rem;
-    border-radius: 8px;
-  }
-`
-
-export const Separator = styled.div`
-  padding: 2rem;
-  color: ${(props) => props.theme['red-lighten-3']};
-  width: 4rem;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
+  gap: 2rem;
 `
 
 export const CountDownButton = styled.button`
-  width: 100%;
   border: 0;
   padding: 1rem;
-  border-radius: 8px;
+  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -95,6 +74,7 @@ export const CountDownButton = styled.button`
   font-weight: bold;
   color: ${(props) => props.theme['gray-100']};
   background-color: ${(props) => props.theme['red-lighten-3']};
+  transition: 0.3s;
 
   &:disabled {
     opacity: 0.7;
@@ -103,5 +83,13 @@ export const CountDownButton = styled.button`
 
   &:not(:disabled):hover {
     background-color: ${(props) => props.theme['red-lighten-2']};
+  }
+
+  &.danger {
+    background-color: ${(props) => props.theme['red-500']};
+
+    &:hover {
+      background-color: ${(props) => props.theme['red-lighten-1']};
+    }
   }
 `
